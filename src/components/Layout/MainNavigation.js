0368d829237480e644cf0560b325acdf2fc44744
Suresh-Kumar-Lohar <sureshlohar468@gmail.com>
2022-11-18 17:25:26 +0530

    ecommerce task-16

--- src/components/Layout/MainNavigation.js
+++ src/components/Layout/MainNavigation.js
@@ -1,8 +1,12 @@
-import { Link } from 'react-router-dom';
-
-import classes from './MainNavigation.module.css';
+import { Link } from 'react-router-dom'
+import { useContext } from 'react'
+import AuthContext from '../../store/auth-context'
+import classes from './MainNavigation.module.css'
 
 const MainNavigation = () => {
+  const authVtx = useContext(AuthContext)
+
+  const isLoggedIn = authVtx.isLoggedIn
   return (
     <header className={classes.header}>
       <Link to='/'>
@@ -10,19 +14,26 @@ const MainNavigation = () => {
       </Link>
       <nav>
         <ul>
-          <li>
-            <Link to='/auth'>Login</Link>
-          </li>
-          <li>
-            <Link to='/profile'>Profile</Link>
-          </li>
-          <li>
-            <button>Logout</button>
-          </li>
+          {!isLoggedIn && (
+            <li>
+              <Link to='/auth'>Login</Link>
+            </li>
+          )}
+          {isLoggedIn && (
+            <li>
+              <Link to='/profile'>Profile</Link>
+            </li>
+          )}
+
+          {isLoggedIn && (
+            <li>
+              <button onClick={() => authVtx.logout()}>Logout</button>
+            </li>
+          )}
         </ul>
       </nav>
     </header>
-  );
-};
+  )
+}
 
-export default MainNavigation;
+export default MainNavigation

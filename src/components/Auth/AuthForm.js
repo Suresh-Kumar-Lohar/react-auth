cba43fda29813e95df0a6f0d17ff1bfa2762b0df
Suresh-Kumar-Lohar <sureshlohar468@gmail.com>
2022-11-18 17:07:03 +0530

    ecommerce task-15

--- src/components/Auth/AuthForm.js
+++ src/components/Auth/AuthForm.js
@@ -19,36 +19,45 @@ const AuthForm = () => {
     const enteredPassword = passwordInputRef.current.value
     console.log(enteredEmail, enteredPassword)
     setIsLoading(true)
+    let url
     if (isLogin) {
+      url =
+        'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyAVz545erTZB78i1xj1UEkV64Wt0UBKjRA'
     } else {
-      fetch(
-        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAVz545erTZB78i1xj1UEkV64Wt0UBKjRA',
-        {
-          method: 'POST',
-          body: JSON.stringify({
-            email: enteredEmail,
-            password: enteredPassword,
-            returnSecureToken: true,
-          }),
-          headers: {
-            'Content-Type': 'application/json',
-          },
-        }
-      ).then((res) => {
+      url =
+        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAVz545erTZB78i1xj1UEkV64Wt0UBKjRA'
+    }
+    fetch(url, {
+      method: 'POST',
+      body: JSON.stringify({
+        email: enteredEmail,
+        password: enteredPassword,
+        returnSecureToken: true,
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then((res) => {
         setIsLoading(false)
         if (res.ok) {
-          console.log(res)
+          return res.json()
         } else {
           return res.json().then((data) => {
             let errorMessage = 'Authentication failed'
-            if (data && data.error && data.error.message) {
-              errorMessage = data.error.message
-            }
-            alert(errorMessage)
+            // if (data && data.error && data.error.message) {
+            //   errorMessage = data.error.message
+            // }
+            throw new Error(errorMessage)
           })
         }
       })
-    }
+      .then((data) => {
+        console.log(data)
+      })
+      .catch((err) => {
+        alert(err.message)
+      })
   }
 
   return (
